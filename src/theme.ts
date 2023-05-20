eac4186dcfd8a24f5e2452d6aeb6586309e9741e
이영우 <zestlee1106@gmail.com>
2023-05-20 22:38:35 +0900

    [feat] theme 추가

--- src/theme.ts
+++ src/theme.ts
@@ -1,12 +1,33 @@
-import { DefaultTheme } from "styled-components";
+import { createTheme } from "@mui/material/styles";
 
-export const lightTheme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
-};
-export const darkTheme: DefaultTheme = {
-  bgColor: "black",
-  textColor: "white",
-  btnColor: "teal",
-};
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#161616",
+    },
+    secondary: {
+      main: "#EEFD53",
+    },
+    error: {
+      main: "#DA1E28",
+    },
+  },
+  typography: {
+    fontFamily: [
+      "Noto Sans KR",
+      "sans-serif",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      "Segoe UI",
+      "Roboto",
+      "Oxygen",
+      "Ubuntu",
+      "Cantarell",
+      "Fira Sans",
+      "Droid Sans",
+      "Helvetica Neue",
+    ].join(","),
+  },
+});
+
+export default theme;
