eb815d2290381a95941209fd7c1c867952bcc2ac
이영우 <zestlee1106@gmail.com>
2023-05-20 22:38:44 +0900

    [feat] app drawer 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  Drawer,
+  styled,
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import { GroupAdd, Groups, Person2 } from "@mui/icons-material";
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+}));
 
 export default function App() {
+  const [isShow, setIsShow] = React.useState(false);
+  const toggleDrawer = () => {
+    setIsShow((isShow) => !isShow);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -17,14 +38,47 @@ export default function App() {
             edge="start"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 2 }}
+            onClick={toggleDrawer}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <Drawer
+            anchor="left"
+            open={isShow}
+            onClose={toggleDrawer}
+            sx={{
+              "& .MuiDrawer-paper": {
+                width: "80%",
+                boxSizing: "border-box",
+              },
+            }}
+          >
+            <DrawerHeader>여기 아이콘 자리</DrawerHeader>
+            <Divider />
+            <List>
+              <ListItem>
+                <ListItemIcon>
+                  <Person2 />
+                </ListItemIcon>
+                <ListItemText primary={"로그인"}></ListItemText>
+              </ListItem>
+            </List>
+            <Divider />
+            <List>
+              <ListItem>
+                <ListItemIcon>
+                  <GroupAdd />
+                </ListItemIcon>
+                <ListItemText primary={"파티개설"}></ListItemText>
+              </ListItem>
+              <ListItem>
+                <ListItemIcon>
+                  <Groups />
+                </ListItemIcon>
+                <ListItemText primary={"파티목록"}></ListItemText>
+              </ListItem>
+            </List>
+          </Drawer>
         </Toolbar>
       </AppBar>
     </Box>
